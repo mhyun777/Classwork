114cb70b6b4dd391c27512e12c762432254bce94
Fern Hyun <mhyun@haverford.edu>
2026-01-29 10:36:34 -0500

    setters & getters

--- classwork/src/main/java/classwork/Course.java
+++ classwork/src/main/java/classwork/Course.java
@@ -1,17 +1,45 @@
 package classwork;
 
 public class Course {
-    public String name;
-    public enum Department {CMSC,};
-    public Department department;
+    private String name;
+    public enum Department {CMSC, CNSE};
+    private Department department;
     public enum College {H, B, S};
-    public College college;
-    public int number;
+    private College college;
+    private int number;
 
     public Course(String name, Department department, College college, int number) {
         this.name = name;
         this.department = department;
         this.college = college;
+        this.number = number;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Department getDepartment() {
+        return this.department;
+    }
+    public void setDepartment(Department department) {
+        this.department = department;
+    }
+
+    public College getCollege() {
+        return this.college;
+    }
+    public void setCollege(College college) {
+        this.college = college;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+    public void setNumber(int number) {
         this.number = number;
     }
 
@@ -24,6 +52,10 @@ public class Course {
         switch (department) {
             case CMSC: {
                 output += "CMSC ";
+                break;
+            }
+            case CNSE: {
+                output += "CNSE ";
                 break;
             }
             default: {
--- classwork/src/main/java/classwork/Main.java
+++ classwork/src/main/java/classwork/Main.java
@@ -6,10 +6,21 @@ public class Main {
     }
 
     public static void testCourseClass() {
-        Course dataStructures = new Course("Intro to Data Structures", Course.Department.CMSC, Course.College.H, 106);
-        System.out.println("Data structures's code is: ");
-        System.out.println(dataStructures.code());
-        System.out.println("and its full name is: ");
-        System.out.println(dataStructures);
+        Course dataStructures = new Course("Date Structres", Course.Department.CNSE, Course.College.B, 306);
+        System.out.println("Data Structures' Data: ");
+        System.out.println("\t" + dataStructures.getName());
+        System.out.println("\t" + dataStructures.getDepartment());
+        System.out.println("\t" + dataStructures.getCollege());
+        System.out.println("\t" + dataStructures.getNumber());
+
+        dataStructures.setName("Intro to Data Structures");
+        dataStructures.setDepartment(Course.Department.CMSC);
+        dataStructures.setCollege(Course.College.H);
+        dataStructures.setNumber(106);
+
+        System.out.println("Data Structures' Data (Fixed): ");
+        System.out.println("\tCode: " + dataStructures.code());
+        System.out.println("\tFull Name: " + dataStructures);
+
     }
 }
